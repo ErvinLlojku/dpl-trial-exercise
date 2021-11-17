06fbfbc656555956817c455aa577a1cac3caafec
Ervin Llojku <llojku.ervin@gmail.com>
2021-11-17 21:40:25 +0100

    Add mock for birthday query

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,15 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./scheme");
 
-const server = new ApolloServer({ typeDefs, mocks: true })
+const mocks = {
+  Birthday: () => ({
+    day: () => 13,
+    month: () => 6,
+    year: () => 2021
+  })
+};
+
+const server = new ApolloServer({ typeDefs, mocks })
 
 server.listen({ port: 5000 }).then(() => {
   console.log(`
